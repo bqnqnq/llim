70ea13f321183f3654a22828ce60ebea861b9c17
bqnqnq <ncnom@hotmail.com>
2025-08-05 11:08:00 +0900

    feat: main.rsでコマンドを受け付ける

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(long)]
+    start: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.start {
+        println!("Starting...")
+    } else {
+        println!("Use --start to begin.")
+    }
 }
